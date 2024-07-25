9c3544601a6c08605a6565bbbc6fd06059e7bbba
Bunma93 <bunma93@gmail.com>
2024-07-26 05:48:13 +0700

    Js-arr50

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+const scores = [
+    { score: 90, subject: 'HTML', weight: 0.2 },
+    { score: 95, subject: 'CSS', weight: 0.3 },
+    { score: 85, subject: 'JavaScript', weight: 0.5 }
+  ];
+
+let score = 0;
+
+for (let num of scores) {
+    score += (num.score*num.weight);
+}
+
+console.log(score);
